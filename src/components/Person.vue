a722e66f9b5a39425a342ba942074132584bf381
LiHongFei <2529574569@qq.com>
2024-12-03 23:01:32 +0800

    ref和reactive创建响应式数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,44 +1,47 @@
 <template>
     <div class="person">
-        <!-- <h2>{{ a }}</h2> -->
-        <h2>姓名： {{name}}</h2>
-        <h2>年龄： {{age}}</h2>
-        <h2>地址： {{address}}</h2>
-        <button @click="changeName">修改名字</button>
-        <button @click="changeAge">修改年龄</button>
-        <button @click="showTel">查看联系方式</button>
+        <h2>汽车信息：一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
+        <button @click="changeBrand">修改汽车的品牌</button>
+        <button @click="changePrice">修改汽车的价格</button>
+        <button @click="changeCar">修改汽车</button>
+        <hr>
+        <h2>当前求和为：{{ sum }}</h2>
+        <button @click="changeSum">点我sum+1</button>
     </div>
 </template>
 
-<!-- <script lang="ts">
-    export default {
-        name: 'Person',
-    }
-</script> -->
+<script lang="ts" setup name="Person">
+    import {ref,reactive} from 'vue';
 
-<script lang="ts" setup name>
-    //数据，原来是写在data中的，此时的name、age、tel都不是响应式的数据
-    let name = '张三';  //注意此时的name不是响应式的
-    let age = 18;  // 注意此时的age不是响应式的
-    let tel = '12388888888';  // 注意此时的tel不是响应式的
-    // let x = this.d;
-    let address = '北京昌平区宏福苑.宏福科技园'
+    // 数据
+    let car = reactive({brand:'奔驰',price:100})
+    // let car = ref({brand:'奔驰',price:100})
+    let sum = ref(0)
 
     // 方法
-    function changeName() {
-        name = 'zhang-san'  // 注意：这样修改name，页面是没有变化的
-        console.log(name)  // name确实改了，但name不是响应式的
+    function changeBrand() {
+        car.brand = '宝马'
+        // car.value.brand = '宝马'
     }
-    function changeAge() {
-        age += 1  // 注意：这样修改age，页面是没有变化的
-        console.log(age)  // age确实改了，但age不是响应式的
+    function changePrice() {
+        car.price += 10
+        // car.value.price += 10
     }
-    function showTel() {
-        alert(tel)
+    function changeCar() {
+        // car = {brand: '奥托',price: 1} // 这么写页面不更新的
+        // car = reactive({brand: '奥托',price: 1}) // 这么写页面不更新的
+        
+        // 下面这个写法页面可以更新
+        Object.assign(car, {brand: '奥托',price: 1})
+        // car.value = {brand: '奥托',price: 1}
+    }
+    function changeSum() {
+        sum.value += 1
+        // sum = ref(9)
     }
 </script>
 
-<style>
+<style scoped>
   .person {
     background-color: skyblue;
     box-shadow: 0 0 10px;
@@ -47,5 +50,8 @@
   }
   button {
     margin: 0 5px;
+  }
+  li {
+    font-size: 20px;
   }
 </style>
