56795723ed772f9e6451b7b7fdb55f33ab29c0b8
LiHongFei <2529574569@qq.com>
2024-12-15 21:56:31 +0800

    其他组件使用案例

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div class="outer">
-    <h2>我是App组件</h2>
-    <img src="https://www.atguigu.com/images/index_new/logo.png" alt="">
-    <br>
-    <Modal/>
-  </div>
+    <div class="app">
+        <h2>我是App组件{{ x }}</h2>
+        <Hello/>
+        <Child/>
+    </div>
 </template>
 
 <script setup lang="ts" name="App">
-    import Modal from './Modal.vue';
+    import Child from './Child.vue';
 </script>
 
 <style scoped>
-    .outer {
+    .app {
         background-color: #ddd;
         border-radius: 10px;
-        padding: 5px;
+        padding: 10px;
         box-shadow: 0 0 10px;
-        width: 400px;
-        height: 400px;
-        /* filter: saturate(200%); */
-    }
-    img {
-        width: 270px;
     }
 </style>
